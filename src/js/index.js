4a5104b5cdbf15e47c7e354f5e7c52b8b5d81daf
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-25 17:47:46 +0200

    goit-js-hw-10

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// import SlimSelect from 'slim-select'
+
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Notify.failure('Oops! Something went wrong! Try reloading the page!')
+            
+// new SlimSelect({
+//   select: '#selectElement'
+// })

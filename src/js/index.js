a915d96d3db3838cf3e6f494de041fcd1c66542d
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-30 18:13:19 +0200

    hw-10

--- src/js/index.js
+++ src/js/index.js
@@ -1,57 +1,60 @@
-import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// Notify.failure('Oops! Something went wrong! Try reloading the page!')
-            
+const breedList = document.querySelector('.breed-select');
+const CatInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+// const error = document.querySelector('.error');
 
+breedList.addEventListener('change', handlerSelect);
 
-const breedList = document.querySelector(".breed-select");
-const catInfo = document.querySelector(".cat-info");
-const loaderEl = document.querySelector(".loading");
-
-loaderEl.classList.remove('is-hidden');
-breedList.classList.remove('is-hidden');
+Loading.circle('Loading data, please wait...');
 
 fetchBreeds()
     .then(data => {
-        createOptions(data);
+        createOptions(data.data);
         new SlimSelect({
             select: '.breed-select',
         });
-        // breedList.classList.remove('is-hidden');
-        // loaderEl.classList.add('is-hidden')
+        breedList.classList.remove('is-hidden');
     })
-    .catch((error) => console.log(error))
-    .finally(loaderEl.classList.add("is-hidden"));
+    .catch(error => {
+        Report.failure(
+                'Oops! Something went wrong!',
+                '🐾  Try reloading the page!',
+                'OK'
+            );
+    })
+    .finally(Loading.remove());
 
-function createOptions(arrCats) { 
+function createOptions(arrCats) {
     breedList.innerHTML = arrCats
         .map(({ id, name }) => `<option value=${id}>${name}</option>`)
-        .join("");
+        .join('');
 }
 
-function createMarkup(selectedCat) {
-    catInfo.innerHTML = `<article class="flex-container">
-  <img src="${selectedCat.url}" alt="${selectedCat.breeds[0].name}" width="300">
-  <div class="thumb">
-    <h3>${selectedCat.breeds[0].name}</h3>
-    <p>${selectedCat.breeds[0].description}</p>
-    <p><b>Temperament: </b>${selectedCat.breeds[0].temperament}</p>
-  </div></article>`;
+function createCard(selectedCat) {
+    CatInfo.innerHTML = `<article class="flex-container"><img src=${selectedCat.url} alt=${selectedCat.breeds[0].name} width="300"><div class="thumb"><h3>${selectedCat.breeds[0].name}</h3><p>${selectedCat.breeds[0].description}</p><p><b>Temperament: </b>${selectedCat.breeds[0].temperament}</p></div></article>`;
 }
 
-function handlerSelect(elem) { 
-    loaderEl.classList.remove("is-hidden");
-    catInfo.innerHTML = "";
+function handlerSelect(elem) {
+    Loading.circle('Loading data, please wait...');
+    CatInfo.innerHTML = '';
     fetchCatByBreed(elem.target.value)
         .then(data => {
-            // createMarkup(...data.data);
-            createMarkup(data[0]);
-            catInfo.classList.remove("is-hidden");
+            createCard(...data.data);
+            CatInfo.classList.remove('is-hidden');
         })
-        .catch((error) => console.log(error))
-        .finally(() => loaderEl.classList.add("is-hidden"));
+        .catch(error => {
+            Report.failure(
+                'Oops! Something went wrong!',
+                '🐾  Try reloading the page!',
+                'OK'
+            );
+        })
+        .finally(Loading.remove());
 }
 

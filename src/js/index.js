68f9bdc55da807896c749f0c5c0bef7c0383b223
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-30 14:32:50 +0200

    hw-10

--- src/js/index.js
+++ src/js/index.js
@@ -5,45 +5,53 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 // Notify.failure('Oops! Something went wrong! Try reloading the page!')
             
-// new SlimSelect({
-//   select: '#selectElement'
-// })
+
 
 const breedList = document.querySelector(".breed-select");
 const catInfo = document.querySelector(".cat-info");
 const loaderEl = document.querySelector(".loading");
 
-breedList.addEventListener("change", handlerSelect);
+loaderEl.classList.remove('is-hidden');
+breedList.classList.remove('is-hidden');
 
-//*========
+fetchBreeds()
+    .then(data => {
+        createOptions(data);
+        new SlimSelect({
+            select: '.breed-select',
+        });
+        // breedList.classList.remove('is-hidden');
+        // loaderEl.classList.add('is-hidden')
+    })
+    .catch((error) => console.log(error))
+    .finally(loaderEl.classList.add("is-hidden"));
 
+function createOptions(arrCats) { 
+    breedList.innerHTML = arrCats
+        .map(({ id, name }) => `<option value=${id}>${name}</option>`)
+        .join("");
+}
 
-
-//*========
+function createMarkup(selectedCat) {
+    catInfo.innerHTML = `<article class="flex-container">
+  <img src="${selectedCat.url}" alt="${selectedCat.breeds[0].name}" width="300">
+  <div class="thumb">
+    <h3>${selectedCat.breeds[0].name}</h3>
+    <p>${selectedCat.breeds[0].description}</p>
+    <p><b>Temperament: </b>${selectedCat.breeds[0].temperament}</p>
+  </div></article>`;
+}
 
 function handlerSelect(elem) { 
     loaderEl.classList.remove("is-hidden");
     catInfo.innerHTML = "";
     fetchCatByBreed(elem.target.value)
         .then(data => {
-            createCard(...data.data);
+            // createMarkup(...data.data);
+            createMarkup(data[0]);
             catInfo.classList.remove("is-hidden");
         })
-        .catch(error => error)
-    .finally(loading.remove("is-hidden"))
+        .catch((error) => console.log(error))
+        .finally(() => loaderEl.classList.add("is-hidden"));
 }
-
-function createMarkup(selectedCat) {
-    catInfo.innerHTML = `<article class="flex-container">
-  <img src=${selectedCat.url} alt=${selectedCat.breeds[0].name} width="300">
-  <div class="thumb">
-    <h3>${selectedCat.breeds[0].name}</h3>
-    <p>${selectedCat.breeds[0].description}</p>
-    <p><b>Temperament: </b>${selectedCat.breeds[0].temperament}</p>
-  </div></article>`
-}
-
-
-
-
 
